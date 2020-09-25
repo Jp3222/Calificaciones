263ca4aac92e3dac656085d2ac998b2ea43a784a
Juan Pablo Campos <64780248+Jp3222@users.noreply.github.com>
2020-09-25 06:26:02 -0500

    Create Logica.java

---
+++ Logica/Logica.java
@@ -0,0 +1,42 @@
+package Logica;
+public class Logica {
+
+    public double PromedioGrupal(double[] Calif) {
+        double Promedio = 0;
+        for (int i = 0; i < Calif.length; i++) {
+            Promedio += Calif[i];
+        }
+        Promedio = Promedio / Calif.length;
+        return Promedio;
+    }
+
+    public void VerDatos(String[] Nombre, int[] NoControl, double[] Calif) {
+        for (int i = 0; i < Calif.length; i++) {
+            System.out.print("[" + (i + 1) + "] Numero de control: " + NoControl[i]
+                    + "\nAlumno: " + Nombre[i] + "\nCalificacion: " + Calif[i]);
+            System.out.println("");
+
+        }
+    }
+
+    //Metodo para Mostrar los primeros 5 promedio de todos
+    public void VerPrimerosDatos(String[] Nombre, int[] NoControl, double[] Calif) {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("[" + (i + 1) + "] Numero de control: " + NoControl[i]
+                    + "\nAlumno: " + Nombre[i] + "\nCalificacion: " + Calif[i]);
+            System.out.println("");
+
+        }
+    }
+
+    //Metodo para Mostrar los primeros 5 promedio de todos
+    public void VerUltimosDatos(String Nombre[], int NoControl[], double Calif[]) {
+        System.out.println("Ver los ultimos 5 alumnos\n");
+        for (int i = Calif.length - 1; i > 10; i--) {
+            System.out.print("[" + (i + 1) + "] Numero de control:" + NoControl[i]
+                    + "\nAlumno: " + Nombre[i]
+                    + "\nCalificacion: " + Calif[i]);
+            System.out.println("\n");
+        }
+    }
+}
